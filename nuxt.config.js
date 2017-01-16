9db19c6a2bf5c712c8f216f149b449494bdcbbb4
Jikkai Xiao <sonne@asaki.me>
2017-01-16 18:53:19 +0800

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'icon', href: 'favicon.png' }
     ]
   },
   css: [
