c3b8cc5161c855797c42e9d61ebc3d8a80072241
Jikkai Xiao <sonne@asaki.me>
2017-01-16 16:57:13 +0800

    add floatball component

---
+++ components/floatball.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    created () {
+      if (typeof window !== 'undefined') {
+        console.log(this.$el)
+      }
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,17 @@
 <template>
   <div>
-
+    <g-floatball />
     <g-footer />
   </div>
 </template>
 
 <script>
+  import Floatball from '~components/floatball'
   import Footer from "~components/footer"
 
   export default {
-    data () {
-      return {
-        profile: {
-          title: 'Gratitude',
-          subtitle: 'No Past No future'
-        }
-      }
-    },
     components: {
+      GFloatball: Floatball,
       GFooter: Footer
     }
   }
