4132d6bb79a30c47ea313c6e162385eabee592ab
jikkai <sonne@asaki.me>
2017-07-20 00:05:37 +0800

    add style complie

--- config/config.default.js
+++ config/config.default.js
@@ -1,3 +1,16 @@
-exports.config = {
-  title: 'Period'
+module.exports = {
+  title: 'Period',
+  since: 2016,
+  now: new Date().getFullYear(),
+  root: '',
+  meta: [
+    { name: 'keywords', content: '' },
+    { name: 'description', content: '' },
+    { name: 'author', content: '十界' }
+  ],
+  link: [
+    { rel: 'shortcut icon', href: '/static/favicon.png' },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Satisfy' },
+    { rel: 'stylesheet', href: '/static/app.css' }
+  ]
 }
