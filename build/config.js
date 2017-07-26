7906c6ddc34b184c59f5797ad141ad5bb4f83243
jikkai <sonne@asaki.me>
2017-07-27 01:15:18 +0800

    create new demo

---
+++ build/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  port: '6928',
+  title: 'Period',
+  vendor: [
+    'react',
+    'react-dom'
+  ],
+  cssModules: false
+}
---
+++ build/webpack.base.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const config = require('./config')
+
+module.exports = {
+  entry: {
+    client: './src/index.tsx'
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].js',
+    publicPath: './'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.css', '.json'],
+    alias: {
+      '~': path.join(__dirname, '../src')
+    }
+  },
+  performance: {},
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        use: 'source-map-loader'
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'awesome-typescript-loader'
+      },
+      {
+        test: /\.(ico|jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: 'file-loader?limit=8192'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: config.title,
+      favicon: __dirname + '/favicon.png',
+      template: __dirname + '/index.html',
+      filename: './index.html'
+    })
+  ]
+}
